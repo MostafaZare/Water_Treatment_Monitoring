5c1f06afb360692531cb4873ee61facecc04d884
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 20:51:49 +0330

    Create actuator.py

---
+++ actuator.py
@@ -0,0 +1,83 @@
+# actuator.py
+import logging
+
+class Actuator:
+    def __init__(self, actuator_id, actuator_type, initial_config=None):
+        self.actuator_id = actuator_id
+        self.actuator_type = actuator_type
+        self.config = initial_config or {}
+        self.logger = logging.getLogger(f"{actuator_type}_{actuator_id}")
+        self.is_operational = False
+
+    def activate(self):
+        """
+        Activate the actuator.
+        This method should be overridden by subclasses to implement
+        the actual activation logic specific to the actuator type.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def deactivate(self):
+        """
+        Deactivate the actuator.
+        This method should be overridden by subclasses to implement
+        the actual deactivation logic specific to the actuator type.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def configure(self, new_config):
+        """
+        Configure the actuator with new parameters.
+        """
+        self.config.update(new_config)
+        self.logger.info(f"Actuator {self.actuator_id} reconfigured with: {new_config}")
+
+    def check_status(self):
+        """
+        Check the operational status of the actuator.
+        """
+        # Placeholder for actual status check logic
+        try:
+            # Simulate status check
+            self.is_operational = True
+            self.logger.info(f"Actuator {self.actuator_id} is operational.")
+        except Exception as e:
+            self.is_operational = False
+            self.logger.error(f"Actuator {self.actuator_id} is not operational: {e}")
+
+    def reset(self):
+        """
+        Reset the actuator to its initial state/configuration.
+        """
+        # Placeholder for actual reset logic
+        self.logger.info(f"Actuator {self.actuator_id} reset to initial configuration.")
+        self.configure(self.config)
+
+# Example subclass for a specific actuator type
+class ValveActuator(Actuator):
+    def activate(self):
+        """
+        Override the activate method to open the valve.
+        """
+        # Placeholder for actual activation logic
+        self.logger.info(f"Valve {self.actuator_id} opened.")
+
+    def deactivate(self):
+        """
+        Override the deactivate method to close the valve.
+        """
+        # Placeholder for actual deactivation logic
+        self.logger.info(f"Valve {self.actuator_id} closed.")
+
+# Example usage
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    valve = ValveActuator(actuator_id=1, actuator_type="Valve", initial_config={"position": "closed"})
+    valve.check_status()
+
+    if valve.is_operational:
+        valve.activate()
+        # Do something while the valve is active
+        valve.deactivate()
+    else:
+        logging.error("Actuator is not operational.")

ad18d3e02b5d20c4b7810c25fab3db8e0227c244
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 18:53:10 +0330

    Create radar_sensor.py

---
+++ radar_sensor.py
@@ -0,0 +1,52 @@
+# radar_sensor.py
+from modbus_lib import ModbusDevice
+from device_interface import DeviceInterface
+
+class RadarSensor(DeviceInterface):
+    # Assuming we have registers defined for radar sensor
+    RADAR_DISTANCE_REGISTER = 100  # Example register address for radar distance
+
+    def __init__(self, port, slave_id, baudrate=9600):
+        self.modbus_device = ModbusDevice(port, slave_id, baudrate)
+    
+    def read_distance(self):
+        """Reads the distance measured by the radar sensor."""
+        try:
+            # Read a single register that holds the distance value
+            distance_register_values = self.modbus_device.read_registers(self.RADAR_DISTANCE_REGISTER, 1)
+            if distance_register_values:
+                # Assuming the distance is in the first register and no conversion is needed
+                return distance_register_values[0]
+            else:
+                return None
+        except Exception as e:
+            print(f"Error reading radar sensor distance: {e}")
+            return None
+
+    def close(self):
+        """Closes the connection to the sensor."""
+        self.modbus_device.close()
+
+# Example usage
+if __name__ == '__main__':
+    # Configuration for the radar sensor
+    PORT = '/dev/ttyUSB0'
+    SLAVE_ID = 1
+    
+    # Initialize the radar sensor
+    radar_sensor = RadarSensor(PORT, SLAVE_ID)
+
+    # Read the distance value from the sensor
+    distance = radar_sensor.read_distance()
+    if distance is not None:
+        print(f"Radar distance reading: {distance} units")
+    else:
+        print("Failed to read distance")
+
+    # Clean up and close the connection
+    radar_sensor.close()
+
+
+"""Please replace the placeholder values and register addresses with the correct ones according to your radar sensor's documentation. 
+The DeviceInterface should define the structure that all device classes follow, including methods like read_data, write_data, etc., 
+that you would call on your RadarSensor instances."""

b4e043099f157b884d3e33d2b944bad6f4a52313
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 16:43:58 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 "The __init__.py file in Python is used to mark directories on disk as a Python package directory.
 "If you have a directory containing a __init__.py file, when you import the package in a Python script, the __init__.py executes and defines what symbols the package exposes to the outside world.
-"you can put initialization code for the package in this file.
+"you can put the initialization code for the package in this file.
 "Below is an example of a simple __init__.py which might also include importing certain modules from the package for easier access:
 
 # __init__.py
@@ -16,3 +16,4 @@ print("Package initialized.")
 "If you are using Python 3.3 or above, __init__.py files are no longer required to define a directory as a Python package, 
 "but they are still used to define what gets imported when import * is called on a package. 
 "So, it's often still a good practice to include them.
+"If you don't need any package-level initialization or symbols definition, then simply creating an empty file named __init__.py is sufficient"

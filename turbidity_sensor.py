ecb0ff10abdd19f3f1d566ec3d286aced3497866
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 20:23:50 +0330

    Create turbidity_sensor.py

---
+++ turbidity_sensor.py
@@ -0,0 +1,51 @@
+# turbidity_sensor.py
+from modbus_lib import ModbusDevice
+from device_interface import DeviceInterface
+
+class TurbiditySensor(DeviceInterface):
+    # Assuming we have a register defined for turbidity value
+    TURBIDITY_REGISTER = 101  # Example register address for turbidity
+
+    def __init__(self, port, slave_id, baudrate=9600):
+        self.modbus_device = ModbusDevice(port, slave_id, baudrate)
+
+    def read_turbidity(self):
+        """Reads the turbidity value from the sensor."""
+        try:
+            # Read a single register that holds the turbidity value
+            turbidity_register_values = self.modbus_device.read_registers(self.TURBIDITY_REGISTER, 1)
+            if turbidity_register_values:
+                # Assuming the turbidity is in the first register and no conversion is needed
+                return turbidity_register_values[0]
+            else:
+                return None
+        except Exception as e:
+            print(f"Error reading turbidity sensor value: {e}")
+            return None
+
+    def close(self):
+        """Closes the connection to the sensor."""
+        self.modbus_device.close()
+
+# Example usage
+if __name__ == '__main__':
+    # Configuration for the turbidity sensor
+    PORT = '/dev/ttyUSB1'
+    SLAVE_ID = 2
+    
+    # Initialize the turbidity sensor
+    turbidity_sensor = TurbiditySensor(PORT, SLAVE_ID)
+
+    # Read the turbidity value from the sensor
+    turbidity = turbidity_sensor.read_turbidity()
+    if turbidity is not None:
+        print(f"Turbidity reading: {turbidity} NTU")
+    else:
+        print("Failed to read turbidity")
+
+    # Clean up and close the connection
+    turbidity_sensor.close()
+
+"""This example assumes that the turbidity sensor's data can be read from a Modbus register, 
+and that it communicates over RS485 or a similar interface. 
+The register addresses and how we handle the data will depend on the specific turbidity sensor model we are using."""

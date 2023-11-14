e12b8d8872704a63c512727bad2dc300eba6ea47
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 16:51:18 +0330

    Update thingsboard_client.py

--- thingsboard_client.py
+++ thingsboard_client.py
@@ -1,49 +1,126 @@
-# thingsboard_client.py
+"""The thingsboard_client.py file would be responsible for handling communication with the ThingsBoard platform, 
+including publishing telemetry data, handling attribute updates, and processing RPC calls."""
+"""Here is a more comprehensive thingsboard_client.py module with expanded capabilities, including error handling, attribute updates, and RPC response handling"""
+"""In this version, the ThingsBoardClient class includes:
 
-import time
+Error handling for connection issues.
+An on_message callback that differentiates between RPC requests and attribute updates.
+A handle_rpc_request method to process incoming RPC requests.
+A respond_to_rpc method to send RPC responses.
+A handle_attribute_update method placeholder to manage attribute updates.
+Logging for key actions and errors."""
+
+    
+import paho.mqtt.client as mqtt
 import json
-from tb_gateway_mqtt import TBGatewayMqttClient
+import logging
 
 class ThingsBoardClient:
-    def __init__(self, host, port, access_token):
-        self.client = TBGatewayMqttClient(host, port, access_token)
-        self.client.connect()
-        # Subscribe to necessary topics if required
-        self.client.subscribe_to_attribute_updates(self.on_attribute_update)
-        self.client.set_server_side_rpc_request_handler(self.on_server_side_rpc_request)
+    def __init__(self, server, token, port=1883):
+        self.server = server
+        self.token = token
+        self.port = port
+        self.client = mqtt.Client()
+        self.client.username_pw_set(token)
+        self.connected = False
 
-    def on_attribute_update(self, attribute, value):
-        # Process attribute update from ThingsBoard
-        pass
+        # Set up callback functions
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
 
-    def on_server_side_rpc_request(self, request_id, request_body):
-        # Process server-side RPC request from ThingsBoard
-        pass
+        # Optional: Configure logging
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
-    def send_attributes(self, attributes):
-        # Send attributes to ThingsBoard
-        self.client.gw_send_attributes('default', attributes)
+    def connect(self):
+        try:
+            self.client.connect(self.server, self.port, 60)
+            self.client.loop_start()
+        except Exception as e:
+            self.logger.error(f"Connection failed: {e}")
 
-    def send_telemetry(self, telemetry):
-        # Send telemetry to ThingsBoard
-        self.client.gw_send_telemetry('default', telemetry)
+    def disconnect(self):
+        self.client.disconnect()
+        self.client.loop_stop()
 
-    def request_attributes(self, keys, callback):
-        # Request shared attributes from ThingsBoard
-        self.client.gw_request_shared_attributes('default', keys, callback)
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            self.connected = True
+            self.logger.info("Connected to ThingsBoard")
+            # Subscribe to attribute updates and RPC calls
+            self.client.subscribe(f"v1/devices/me/attributes")
+            self.client.subscribe(f"v1/devices/me/rpc/request/+")
+        else:
+            self.logger.error(f"Connection failed with code {rc}")
 
-    def claim_device(self, device_name, secret_key, duration):
-        # Claim the device on ThingsBoard
-        self.client.gw_claim(device_name, secret_key, duration)
+    def on_message(self, client, userdata, msg):
+        self.logger.info(f"Message received on topic {msg.topic}: {str(msg.payload)}")
+        # Handle incoming messages
+        if msg.topic.startswith('v1/devices/me/rpc/request/'):
+            self.handle_rpc_request(msg)
+        elif msg.topic == 'v1/devices/me/attributes':
+            self.handle_attribute_update(msg)
 
-    def connect_device(self, device_name, device_type="default"):
-        # Connect a new device to ThingsBoard
-        self.client.gw_connect_device(device_name, device_type)
+    def handle_rpc_request(self, msg):
+        # Process RPC request
+        rpc_request = json.loads(msg.payload)
+        request_id = msg.topic.split('/')[-1]
+        method = rpc_request.get('method')
+        params = rpc_request.get('params')
 
-    def disconnect_device(self, device_name):
-        # Disconnect the device from ThingsBoard
-        self.client.gw_disconnect_device(device_name)
+        # Example: Handle RPC method
+        if method == 'getTelemetry':
+            # Implement logic to retrieve telemetry data
+            telemetry_response = self.get_telemetry_data()
+            self.respond_to_rpc(request_id, telemetry_response)
 
-    # Add more methods if needed for handling subscriptions, RPC responses, etc.
+    def get_telemetry_data(self):
+        # Replace with the actual telemetry retrieval logic
+        return {'temperature': 42, 'humidity': 78}
 
-# Additional utility functions or classes can go here
+    def respond_to_rpc(self, request_id, response_data):
+        # Send RPC response
+        self.client.publish(f'v1/devices/me/rpc/response/{request_id}', json.dumps(response_data))
+        self.logger.info(f"RPC Response sent for request ID {request_id}")
+
+    def handle_attribute_update(self, msg):
+        # Process attribute update
+        attribute_update = json.loads(msg.payload)
+        # Implement logic to handle attribute update
+        # ...
+
+    def on_disconnect(self, client, userdata, rc):
+        self.connected = False
+        self.logger.info("Disconnected from ThingsBoard")
+
+    def publish_telemetry(self, telemetry_data):
+        if self.connected:
+            payload = json.dumps(telemetry_data)
+            self.client.publish('v1/devices/me/telemetry', payload)
+            self.logger.info(f"Published telemetry: {payload}")
+        else:
+            self.logger.warning("Client is not connected to ThingsBoard")
+
+# Example usage
+if __name__ == "__main__":
+    ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
+    THINGSBOARD_HOST = "demo.thingsboard.io"
+
+    # Initialize and connect the client
+    tb_client = ThingsBoardClient(THINGSBOARD_HOST, ACCESS_TOKEN)
+    tb_client.connect()
+
+    # Publish some telemetry data
+    telemetry = {'temperature': 42, 'humidity': 78}
+    tb_client.publish_telemetry(telemetry)
+
+    # Keep the script running
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        tb_client.disconnect()
+
+
+

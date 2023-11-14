de23c28e1e15a3ba852bb253b3b9a4dcd22dc7bd
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 17:57:36 +0330

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -4,35 +4,65 @@
 import json
 import os
 
+class ConfigManagerError(Exception):
+    """Custom exception for config manager errors."""
+    pass
+
 class ConfigManager:
-    def __init__(self, config_path='config.json'):
+    def __init__(self, config_path='config/default_config.json'):
         self.config_path = config_path
         self.config_data = self.load_config()
 
     def load_config(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, 'r') as file:
+        """Load the configuration file as a dictionary."""
+        if not os.path.isfile(self.config_path):
+            raise ConfigManagerError(f"Configuration file does not exist: {self.config_path}")
+
+        with open(self.config_path, 'r') as file:
+            try:
                 return json.load(file)
-        else:
-            return {}
+            except json.JSONDecodeError as e:
+                raise ConfigManagerError(f"Error parsing configuration file: {e}")
 
-    def get(self, key, default=None):
-        return self.config_data.get(key, default)
+    def get_config(self, section=None):
+        """
+        Get the configuration dictionary, or a section of it if specified.
+        If the section does not exist, it returns an empty dictionary.
+        """
+        if section:
+            return self.config_data.get(section, {})
+        return self.config_data
 
-    def set(self, key, value):
-        self.config_data[key] = value
+    def update_config(self, section, new_values):
+        """
+        Update the configuration data with new values for a specific section.
+        """
+        if section not in self.config_data:
+            raise ConfigManagerError(f"Section '{section}' not found in the configuration.")
+
+        self.config_data[section].update(new_values)
         self.save_config()
 
     def save_config(self):
+        """Save the current configuration data to the file."""
         with open(self.config_path, 'w') as file:
             json.dump(self.config_data, file, indent=4)
 
-    def remove(self, key):
-        if key in self.config_data:
-            del self.config_data[key]
-            self.save_config()
+    def reload_config(self):
+        """Reload the configuration data from the file."""
+        self.config_data = self.load_config()
 
-# Usage example:
-# config_manager = ConfigManager()
-# some_value = config_manager.get('some_key', default_value)
-# config_manager.set('some_key', new_value)
+# Example usage:
+if __name__ == '__main__':
+    config_manager = ConfigManager()
+
+    # Accessing configuration for a specific section
+    database_config = config_manager.get_config('database')
+
+    # Updating configuration for a specific section
+    new_db_config = {'host': 'localhost', 'port': 5432}
+    config_manager.update_config('database', new_db_config)
+
+    # Reloading configuration from file
+    config_manager.reload_config()
+

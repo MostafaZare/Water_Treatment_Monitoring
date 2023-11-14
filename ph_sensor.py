69c7e2669aebb40489f5deaed6b7812afe4993eb
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 20:30:48 +0330

    Create ph_sensor.py

---
+++ ph_sensor.py
@@ -0,0 +1,84 @@
+""" this is the comprehensive code for a ph_sensor.py module, with enhanced error handling, 
+connection retries, and sensor calibration, would look something like this:"""
+
+# ph_sensor.py
+from modbus_lib import ModbusClient
+from device_interface import DeviceInterface
+import logging
+import time
+
+class PHSensor(DeviceInterface):
+    PH_VALUE_REGISTER = 0x0001  # Example register address for pH value
+    PH_TEMPERATURE_REGISTER = 0x0003  # Example register address for sensor temperature
+    RETRY_ATTEMPTS = 5
+    RETRY_INTERVAL = 2  # seconds
+
+    def __init__(self, port, slave_id, baudrate=9600):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.modbus_client = ModbusClient(port, slave_id, baudrate)
+
+    def read_ph_value(self):
+        """Reads the pH value from the sensor with retries."""
+        for attempt in range(self.RETRY_ATTEMPTS):
+            try:
+                ph_value = self.modbus_client.read_register(self.PH_VALUE_REGISTER)
+                return self.convert_ph_value(ph_value)
+            except Exception as e:
+                self.logger.error(f"Error reading pH value, attempt {attempt + 1}: {e}")
+                time.sleep(self.RETRY_INTERVAL)
+        return None
+
+    def read_temperature(self):
+        """Reads the temperature from the sensor with retries."""
+        for attempt in range(self.RETRY_ATTEMPTS):
+            try:
+                temperature = self.modbus_client.read_register(self.PH_TEMPERATURE_REGISTER)
+                return self.convert_temperature(temperature)
+            except Exception as e:
+                self.logger.error(f"Error reading sensor temperature, attempt {attempt + 1}: {e}")
+                time.sleep(self.RETRY_INTERVAL)
+        return None
+
+    def convert_ph_value(self, raw_value):
+        """Converts raw pH register value to actual pH value."""
+        # Conversion logic goes here based on sensor specifications
+        return raw_value
+
+    def convert_temperature(self, raw_value):
+        """Converts raw temperature register value to actual temperature."""
+        # Conversion logic goes here based on sensor specifications
+        return raw_value
+
+    def calibrate(self, calibration_type, standard_value):
+        """Calibrates the pH sensor."""
+        # Calibration logic goes here based on sensor specifications
+        pass
+
+    def close(self):
+        """Closes the Modbus connection to the sensor."""
+        self.modbus_client.close()
+
+# Example usage
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    PORT = '/dev/ttyUSB0'  # Serial port for pH sensor
+    SLAVE_ID = 1           # Modbus slave ID for the pH sensor
+
+    ph_sensor = PHSensor(PORT, SLAVE_ID)
+
+    ph_value = ph_sensor.read_ph_value()
+    if ph_value is not None:
+        logging.info(f"pH value: {ph_value}")
+    else:
+        logging.error("Failed to read pH value after multiple attempts.")
+
+    temperature = ph_sensor.read_temperature()
+    if temperature is not None:
+        logging.info(f"Sensor temperature: {temperature}")
+    else:
+        logging.error("Failed to read sensor temperature after multiple attempts.")
+
+    # Example calibration process
+    ph_sensor.calibrate('two_point', {'low': 4.0, 'high': 7.0})
+
+    ph_sensor.close()

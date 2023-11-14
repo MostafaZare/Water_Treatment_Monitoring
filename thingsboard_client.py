209dbf669360189cd3264eaa8533faebabbd83b0
Mostafa Zare <mstfazareh@gmail.com>
2023-11-14 18:10:10 +0330

    Update thingsboard_client.py

--- thingsboard_client.py
+++ thingsboard_client.py
@@ -8,120 +8,113 @@ The on_message method has been updated to include a try-except block for robust 
 I removed the redundant on_disconnect callback logic since reconnection is handled within the connect method.
 I've added a dynamic sleep interval after publishing telemetry data in the example usage section. This allows for adjusting """
 
-import paho.mqtt.client as mqtt
 import json
-import logging
 import time
+import logging
+from paho.mqtt import client as mqtt
+
+# Configuration for logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ThingsBoardClient:
-    def __init__(self, server, token, port=1883):
-        self.server = server
-        self.token = token
+    def __init__(self, host, access_token, port=1883):
+        self.host = host
         self.port = port
-        self.client = mqtt.Client()
-        self.client.username_pw_set(token)
-        self.connected = False
+        self.access_token = access_token
+        self.qos = 1  # Default QoS level
 
-        # Set up callback functions
-        self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
-        self.client.on_disconnect = self.on_disconnect
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.username_pw_set(access_token)
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+        self.mqtt_client.on_disconnect = self.on_disconnect
+        self.mqtt_client.on_subscribe = self.on_subscribe
 
-        # Configure logging
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
+        # Reconnect delay
+        self.min_reconnect_delay = 1
+        self.max_reconnect_delay = 120
+        self.current_reconnect_delay = self.min_reconnect_delay
 
     def connect(self):
-        while not self.connected:
-            try:
-                self.client.connect(self.server, self.port, 60)
-                self.client.loop_start()
-                break
-            except Exception as e:
-                self.logger.error(f"Connection failed: {e}, retrying in 5 seconds")
-                time.sleep(5)
-
-    def disconnect(self):
-        self.client.disconnect()
-        self.client.loop_stop()
+        logger.info("Connecting to ThingsBoard...")
+        self.mqtt_client.connect(self.host, self.port, 60)
+        self.mqtt_client.loop_start()
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
-            self.connected = True
-            self.logger.info("Connected to ThingsBoard")
-            # Subscribe to attribute updates and RPC calls
-            client.subscribe(f"v1/devices/me/attributes")
-            client.subscribe(f"v1/devices/me/rpc/request/+")
+            logger.info("Connected to ThingsBoard.")
+            self.mqtt_client.subscribe("v1/devices/me/rpc/request/+", qos=self.qos)
+            self.mqtt_client.subscribe("v1/devices/me/attributes", qos=self.qos)
+            self.current_reconnect_delay = self.min_reconnect_delay
         else:
-            self.logger.error(f"Failed to connect, return code {rc}")
+            logger.error(f"Failed to connect to ThingsBoard: {rc}")
+            self.schedule_reconnect()
+
+    def schedule_reconnect(self):
+        self.mqtt_client.loop_stop()
+        time.sleep(self.current_reconnect_delay)
+        self.current_reconnect_delay = min(self.current_reconnect_delay * 2, self.max_reconnect_delay)
+        self.connect()
+
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        logger.info(f"Subscribed with QoS {granted_qos}")
 
     def on_message(self, client, userdata, msg):
-        try:
-            self.logger.info(f"Message received on topic {msg.topic}")
-            payload = json.loads(msg.payload)
-            if msg.topic.startswith('v1/devices/me/rpc/request/'):
-                self.handle_rpc_request(msg, payload)
-            elif msg.topic == 'v1/devices/me/attributes':
-                self.handle_attribute_update(payload)
-        except json.JSONDecodeError as e:
-            self.logger.error(f"Failed to decode JSON payload: {e}")
-        except Exception as e:
-            self.logger.error(f"Failed to handle message: {e}")
+        logger.info(f"Received message on {msg.topic}: {msg.payload.decode()}")
+        payload = json.loads(msg.payload.decode())
 
-    def handle_rpc_request(self, msg, payload):
-        request_id = msg.topic.split('/')[-1]
+        if msg.topic.startswith('v1/devices/me/rpc/request/'):
+            self.handle_rpc_request(msg.topic, payload)
+
+        elif msg.topic == 'v1/devices/me/attributes':
+            self.handle_attributes_update(payload)
+
+    def handle_rpc_request(self, topic, payload):
+        request_id = topic.split('/')[-1]
+        # Implement your RPC handling logic here
+        response = self.process_rpc_request(payload)
+        self.mqtt_client.publish(f'v1/devices/me/rpc/response/{request_id}', json.dumps(response), qos=self.qos)
+
+    def process_rpc_request(self, payload):
         method = payload.get('method')
         params = payload.get('params')
-        # Handle various RPC methods
+        # Custom RPC handling logic
+        # This is where you would handle different RPC methods
         if method == 'getTelemetry':
-            telemetry_response = self.get_telemetry_data()
-            self.respond_to_rpc(request_id, telemetry_response)
-        # Implement other RPC methods as needed
+            # Return telemetry data
+            return {'temperature': 25.5, 'humidity': 60}
+        else:
+            return {"status": "success", "data": "Unknown method"}
 
-    def get_telemetry_data(self):
-        # Implement actual telemetry retrieval logic
-        return {'temperature': 42, 'humidity': 78}
-
-    def respond_to_rpc(self, request_id, response_data):
-        self.client.publish(f'v1/devices/me/rpc/response/{request_id}', json.dumps(response_data))
-        self.logger.info(f"RPC Response sent for request ID {request_id}")
-
-    def handle_attribute_update(self, payload):
-        # Implement actual attribute update logic
+    def handle_attributes_update(self, payload):
+        # Process attribute update
+        # Implement your attributes update handling logic here
         pass
 
     def on_disconnect(self, client, userdata, rc):
-        self.connected = False
-        self.logger.error("Disconnected from ThingsBoard, attempting to reconnect")
-        self.connect()
-
-    def publish_telemetry(self, telemetry_data):
-        if self.connected:
-            payload = json.dumps(telemetry_data)
-            self.client.publish('v1/devices/me/telemetry', payload)
-            self.logger.info(f"Published telemetry: {payload}")
+        if rc != 0:
+            logger.warning("Unexpected disconnection.")
+            self.schedule_reconnect()
         else:
-            self.logger.warning("Client is not connected to ThingsBoard")
+            logger.info("Disconnected from ThingsBoard.")
+
+    def publish_telemetry(self, telemetry):
+        self.mqtt_client.publish('v1/devices/me/telemetry', json.dumps(telemetry), qos=self.qos)
 
 # Example usage
-if __name__ == "__main__":
+if __name__ == '__main__':
     ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
     THINGSBOARD_HOST = "demo.thingsboard.io"
 
-    # Initialize and connect the client
     tb_client = ThingsBoardClient(THINGSBOARD_HOST, ACCESS_TOKEN)
     tb_client.connect()
 
-    # Publish some telemetry data at a dynamic interval
-    while True:
-        telemetry = {'temperature': 42, 'humidity': 78}
-        tb_client.publish_telemetry(telemetry)
-        # The sleep duration can be dynamically adjusted based on conditions
-        time.sleep(10)  # Example: 10-second interval, can be adjusted as needed
-
-    # The disconnect is handled via KeyboardInterrupt exception
     try:
+        # Publish data periodically
         while True:
-            pass
+            telemetry = {'temperature': 25.5, 'humidity': 60}
+            tb_client.publish_telemetry(telemetry)
+            time.sleep(5)
     except KeyboardInterrupt:
         tb_client.disconnect()

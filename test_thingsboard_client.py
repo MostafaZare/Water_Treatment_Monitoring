2cfb327da2fad4224b2f1c96ce2c453575a14999
Mostafa Zare <mstfazareh@gmail.com>
2023-11-15 20:00:15 +0330

    Create test_thingsboard_client.py

---
+++ test_thingsboard_client.py
@@ -0,0 +1,86 @@
+"""
+Creating a comprehensive test suite for the test_thingsboard_client.py would involve simulating the interactions 
+with the ThingsBoard MQTT API and ensuring that the ThingsBoardClient class can handle various scenarios correctly. """
+
+import unittest
+from unittest.mock import MagicMock
+from thingsboard_client import ThingsBoardClient
+
+class TestThingsBoardClient(unittest.TestCase):
+    def setUp(self):
+        self.host = 'demo.thingsboard.io'
+        self.token = 'ACCESS_TOKEN'
+        self.tb_client = ThingsBoardClient(self.host, self.token)
+        self.tb_client.client = MagicMock()
+
+    def test_connect(self):
+        self.tb_client.connect()
+        self.tb_client.client.connect.assert_called_with(self.host, 1883, 60)
+
+    def test_disconnect(self):
+        self.tb_client.disconnect()
+        self.tb_client.client.disconnect.assert_called()
+
+    def test_publish_telemetry(self):
+        telemetry_data = {'temperature': 25, 'humidity': 60}
+        self.tb_client.publish_telemetry(telemetry_data)
+        self.tb_client.client.publish.assert_called_with(
+            'v1/devices/me/telemetry', '{"temperature": 25, "humidity": 60}'
+        )
+
+    def test_subscribe_to_rpc(self):
+        self.tb_client.subscribe_to_rpc()
+        self.tb_client.client.subscribe.assert_called_with(
+            'v1/devices/me/rpc/request/+'
+        )
+
+    def test_handle_rpc_request(self):
+        # Simulate receiving an RPC request
+        self.tb_client.handle_rpc_request = MagicMock()
+        self.tb_client.on_message(
+            self.tb_client.client,
+            None,
+            self._create_mqtt_message('v1/devices/me/rpc/request/1', '{"method": "getTelemetry"}')
+        )
+        self.tb_client.handle_rpc_request.assert_called()
+
+    def test_respond_to_rpc(self):
+        request_id = 1
+        response_data = {'temperature': 25}
+        self.tb_client.respond_to_rpc(request_id, response_data)
+        self.tb_client.client.publish.assert_called_with(
+            'v1/devices/me/rpc/response/1', '{"temperature": 25}'
+        )
+
+    def test_handle_attribute_update(self):
+        # Simulate receiving an attribute update
+        self.tb_client.handle_attribute_update = MagicMock()
+        self.tb_client.on_message(
+            self.tb_client.client,
+            None,
+            self._create_mqtt_message('v1/devices/me/attributes', '{"firmwareVersion": "1.0.1"}')
+        )
+        self.tb_client.handle_attribute_update.assert_called()
+
+    # Additional methods to test ThingsBoardClient behavior
+    def test_on_connect_success(self):
+        self.tb_client.on_connect(self.tb_client.client, None, None, 0)
+        self.assertTrue(self.tb_client.connected)
+
+    def test_on_connect_failure(self):
+        self.tb_client.on_connect(self.tb_client.client, None, None, 1)
+        self.assertFalse(self.tb_client.connected)
+
+    def _create_mqtt_message(self, topic, payload):
+        message = MagicMock()
+        message.topic = topic
+        message.payload = payload.encode()
+        return message
+
+if __name__ == '__main__':
+    unittest.main()
+
+"""In this test suite, we have mocked the paho.mqtt.client.Client class to prevent actual network operations. 
+The test_subscribe_to_rpc method checks if the client subscribes to the correct topic for RPC requests. 
+The test_handle_rpc_request simulates an incoming RPC request and checks if the method to handle it is called. 
+Similarly, test_respond_to_rpc tests the response to an RPC request, while test_handle_attribute_update simulates an attribute update message from ThingsBoard."""

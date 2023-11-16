ec6ac1d06f5f4807fe4510a6d8e95abfdab0be18
Mostafa Zare <mstfazareh@gmail.com>
2023-11-16 13:34:05 +0330

    Create test_modbus_copy.py

---
+++ test_modbus_copy.py
@@ -0,0 +1,66 @@
+"""
+The test_modbus_copy.py script seems to be a testing utility for Modbus communication with various devices.
+Enhancing this script could involve adding more structured testing procedures, error handling, and logging.
+"""
+
+import logging
+from modbus_lib import DeviceManager
+from radar_sensor import RadarSensor
+from turbidity_sensor import TrubSensor
+from ph_sensor import PHSensor
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# RS485 Communication Configuration
+PORT = '/dev/ttymxc3'
+BAUDRATE = 9600
+PARITY = 'N'
+STOPBITS = 1
+BYTESIZE = 8
+TIMEOUT = 1
+
+# Function to test sensor readings
+def test_sensor_readings(device_manager):
+    # Assuming we have created device classes for each sensor type
+    # Replace these with your actual sensor classes and register addresses
+    radar_sensor = RadarSensor(device_manager, 0x01)
+    trub_sensor = TrubSensor(device_manager, 0x02)
+    ph_sensor = PHSensor(device_manager, 0x03)
+
+    try:
+        radar_reading = radar_sensor.read_data()
+        trub_reading = trub_sensor.read_data()
+        ph_reading = ph_sensor.read_data()
+
+        logger.info(f'Radar Reading: {radar_reading}')
+        logger.info(f'Turbidity Reading: {trub_reading}')
+        logger.info(f'pH Reading: {ph_reading}')
+    except Exception as e:
+        logger.error(f'Error reading sensor data: {e}')
+
+# Main function
+def main():
+    # Set up device manager
+    dev_manager = DeviceManager(PORT, BAUDRATE, PARITY, STOPBITS, BYTESIZE, TIMEOUT)
+    
+    # Add device testing logic
+    test_sensor_readings(dev_manager)
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+In this advanced test script, I've included:
+
+Structured logging using the logging module.
+A test_sensor_readings function that attempts to read from each sensor and logs the results.
+Try-except blocks for handling potential errors during sensor communication.
+Please note that I've assumed the existence of RadarSensor, TrubSensor, and PHSensor classes which handle the specifics of interacting with those devices via Modbus.
+These classes should provide a read_data method that encapsulates the Modbus read logic. 
+The script is designed to be a starting point for building out your testing procedures.
+
+For the projct, you would need to adjust the register addresses and the instantiation of the sensor objects to match the actual devices you are working with. 
+Additionally, you might include more comprehensive tests, assertions, and perhaps integration with a testing framework like unittest or pytest."""
